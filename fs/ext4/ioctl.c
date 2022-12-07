7da152cea4415c4059236fa64f9167826a2b9ec5
Jan Kara <jack@suse.cz>
2022-12-07 12:59:27 +0100

    ext4: initialize quota before expanding inode in setproject ioctl
    commit 1485f726c6dec1a1f85438f2962feaa3d585526f upstream.
    
    Make sure we initialize quotas before possibly expanding inode space
    (and thus maybe needing to allocate external xattr block) in
    ext4_ioctl_setproject(). This prevents not accounting the necessary
    block allocation.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Cc: stable@kernel.org
    Link: https://lore.kernel.org/r/20221207115937.26601-1-jack@suse.cz
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 56836c32a4b5df796b19635b84a7ba4e202ffca9)

--- fs/ext4/ioctl.c
+++ fs/ext4/ioctl.c
@@ -335,6 +335,10 @@ static int ext4_ioctl_setproject(struct file *filp, __u32 projid)
 	if (IS_NOQUOTA(inode))
 		goto out_unlock;
 
+	err = dquot_initialize(inode);
+	if (err)
+		goto out_unlock;
+
 	err = ext4_get_inode_loc(inode, &iloc);
 	if (err)
 		goto out_unlock;
@@ -348,7 +352,6 @@ static int ext4_ioctl_setproject(struct file *filp, __u32 projid)
 	brelse(iloc.bh);
 
 	dquot_initialize(inode);
-
 	handle = ext4_journal_start(inode, EXT4_HT_QUOTA,
 		EXT4_QUOTA_INIT_BLOCKS(sb) +
 		EXT4_QUOTA_DEL_BLOCKS(sb) + 3);
